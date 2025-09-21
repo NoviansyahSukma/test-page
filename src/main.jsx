5bb615e71278517e5b7bf3866286467cdb648cc1
Noviansyah <cymbalavedis@gmail.com>
2025-09-21 15:54:33 +0700

    updated again

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import SitePage from "./components/SitePage";
 import SiteList from "./components/site-list/SiteList";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SitePage />}>
           <Route index element={<SiteList />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
